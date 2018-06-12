ebddc7366a64cbe05d253bcbd75021a06f0b8661
Quan Mai <vimvq1987@gmail.com>
2018-06-12 13:12:02 +0200

    Create GetMetaClassNames.sql

---
+++ GetMetaClassNames.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetMetaClassNames]
+	@EntryIds udttIdTable READONLY,
+	@NodeIds udttIdTable READONLY
+AS
+BEGIN
+	SELECT E.ID, MC.Name as MetaClassName
+	FROM @EntryIds E
+	INNER JOIN CatalogEntry CE ON E.ID = CE.CatalogEntryId
+	INNER JOIN MetaClass MC On CE.MetaClassId = MC.MetaClassId
+
+	SELECT N.ID, MC.Name as MetaClassName
+	FROM @NodeIds N
+	INNER JOIN CatalogNode CN ON N.ID = CN.CatalogNodeId
+	INNER JOIN MetaClass MC On CN.MetaClassId = MC.MetaClassId
+END
